07723e4023c1067a9184e70ab05523d9ff6f6a59
SiMiezz <96351055+SiMiezz@users.noreply.github.com>
2023-01-02 12:29:23 +0100

    fix main

--- Codice/BackEnd/Ratatouille23/src/main/java/com/ingsw/Ratatouille23/Ratatouille23Application.java
+++ Codice/BackEnd/Ratatouille23/src/main/java/com/ingsw/Ratatouille23/Ratatouille23Application.java
@@ -2,14 +2,11 @@ package com.ingsw.Ratatouille23;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.ingsw.Ratatouille23.DAO.*;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Ratatouille23Application {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		SpringApplication.run(Ratatouille23Application.class, args);
 	}
 
