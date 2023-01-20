b28059e237b9521732f62293843e000da75950b0
gianmadd <gi.addati@studenti.unina.it>
2023-01-20 12:30:47 +0100

    interfaccia restaurant stats e calendar (senza logica) + da fixare bug grafico cambio interfaccia in user  fragment
    Nello user fragment, quando un pannello è già in modalità selezione, gli altri pannelli non si comportano correttamente per quanto riguarda il cambio dell'interfaccia e servono più click per cambiarla

--- Codice/FrontEnd/app/src/main/java/com/ingsw/frontend/View/Fragment/RestaurantStatsFragment.java
+++ Codice/FrontEnd/app/src/main/java/com/ingsw/frontend/View/Fragment/RestaurantStatsFragment.java
@@ -1,5 +1,7 @@
 package com.ingsw.frontend.View.Fragment;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.ingsw.frontend.R;
 
@@ -17,6 +21,10 @@ public class RestaurantStatsFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+    private Button searchButton;
+    private Button fromButton;
+    private Button toButton;
 
     public RestaurantStatsFragment() {
         // Required empty public constructor
@@ -44,6 +52,35 @@ public class RestaurantStatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_stats, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_restaurant_stats, container, false);
+
+        searchButton = rootView.findViewById(R.id.restaurant_stats_search_button);
+        fromButton = rootView.findViewById(R.id.restaurant_stats_from_button);
+        toButton = rootView.findViewById(R.id.restaurant_stats_to_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        fromButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog fromDatePicker = new DatePickerDialog(getContext(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+                fromDatePicker.show();
+            }
+        });
+
+        toButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog fromDatePicker = new DatePickerDialog(getContext(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+                fromDatePicker.show();
+            }
+        });
+
+        return rootView;
     }
 }
