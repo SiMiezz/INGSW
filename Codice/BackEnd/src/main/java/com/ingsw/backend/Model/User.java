0cbf18f35ff089e8911c22afee0200cdf90bb798
SiMiezz <96351055+SiMiezz@users.noreply.github.com>
2023-01-21 23:10:18 +0100

    fix default value password

--- Codice/BackEnd/src/main/java/com/ingsw/backend/Model/User.java
+++ Codice/BackEnd/src/main/java/com/ingsw/backend/Model/User.java
@@ -33,7 +33,7 @@ public class User implements Serializable {
 
 
     //ATTRIBUTES
-    @Column(name = "password", columnDefinition="varchar(50) default 'defpwd'", nullable = false)
+    @Column(name = "password",length = 50,nullable = false, insertable = false)
     private String pwd;
 
     @Column(name = "name", length = 50, nullable = false)
