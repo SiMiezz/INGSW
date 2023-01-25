9d2666be36ef60c2e30da6c7b99ceed3a19cf035
gianmadd <gi.addati@studenti.unina.it>
2023-01-25 17:41:57 +0100

    mi ero scordato di pushare questo

---
+++ Codice/FrontEnd/app/src/main/java/com/ingsw/frontend/Presenter/OrderPresenter.java
@@ -0,0 +1,18 @@
+package com.ingsw.frontend.Presenter;
+
+import com.ingsw.frontend.Service.Class.OrderService;
+
+public class OrderPresenter {
+
+    private OrderService orderService;
+
+    public OrderPresenter(){
+        orderService = new OrderService();
+    }
+
+    public void getOrdersByTableId(Integer tableId){
+
+    }
+
+
+}
