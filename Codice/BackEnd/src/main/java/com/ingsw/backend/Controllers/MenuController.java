6269822795f50f4cee4b844bba27a5a44e198de1
SiMiezz <96351055+SiMiezz@users.noreply.github.com>
2023-02-05 11:32:19 +0100

    inizio paginan qrcode

--- Codice/BackEnd/src/main/java/com/ingsw/backend/Controllers/MenuController.java
+++ Codice/BackEnd/src/main/java/com/ingsw/backend/Controllers/MenuController.java
@@ -10,10 +10,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -40,6 +38,13 @@ public class MenuController {
         MenuDTO menuDTO = convertDTO(menu.get());
 
         return menuDTO;
+    }
+
+    @GetMapping("/get/qrcode")
+    public String getById(@RequestParam(name = "id") Integer id, Model model){
+        model.addAttribute("id",id);
+
+        return "qrcode";
     }
 
     private MenuDTO convertDTO(Menu menu) {
